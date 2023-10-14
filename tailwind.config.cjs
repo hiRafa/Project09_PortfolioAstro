cc4fdc6f116618b18c8220a3780b3c86858f2aa6
HIRASHIKI RAFAEL <rafaelkishihiro@gmail.com>
2023-10-15 02:00:14 +0900

    Home Page layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    screens: {
+      'min-2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'min-xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'min-lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'min-md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'min-sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    }
   },
   plugins: [
     function ({ addComponents, theme }) {
