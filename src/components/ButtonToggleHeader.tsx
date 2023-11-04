a6dd961c5a5f2650d1fd06c05f01d4bc581ff0f2
HIRASHIKI RAFAEL <rafaelkishihiro@gmail.com>
2023-11-04 13:27:21 +0900

    header toggle button

--- src/components/ButtonToggleHeader.tsx
+++ src/components/ButtonToggleHeader.tsx
@@ -5,25 +5,19 @@ export const ButtonToggleHeader = () => {
 
   const toggleHeader = () => {
     const header: HTMLElement | null = document.getElementById("myHeader");
-    const toggleButton: HTMLElement | null =
-      document.getElementById("toggleButton");
     const toggleButtonIcon: HTMLElement | null =
       document.getElementById("fa-arrowHeader");
+
     if (!header) return; // Ensure header exists
-    // console.log("icon clicked")
     if (!isHeaderHidden) {
       // Your logic to hide the header
-      // console.log("header is not hidden")
       header.classList.add("css-hideHeader");
-      toggleButton?.classList.remove("rotate");
+      toggleButtonIcon?.classList.remove("rotate");
       // toggleButtonIcon?.classList.remove("rotate");
     } else {
       // Your logic to show the header
-      // console.log("header is hidden")
-      // console.log(toggleButton)
       header.classList.remove("css-hideHeader");
-      toggleButton?.classList.add("rotate");
-      // toggleButtonIcon?.classList.add("rotate");
+      toggleButtonIcon?.classList.add("rotate");
     }
     setIsHeaderHidden(!isHeaderHidden);
   };
@@ -48,8 +42,13 @@ export const ButtonToggleHeader = () => {
       className="hidden absolute bottom-9 -right-6 h-fit w-fit rounded-lg p-2 clr-bg m-auto max-lg:block"
       onClick={() => toggleHeader()}
     >
-     
-      {/* <div class="css-headerButton-lb"></div> */}
+      <p class="hidden absolute bottom-10 right-16 clr-text z-50 text-base max-lg:flex flex-col items-center justify-center -rotate-45">
+        <span>M</span>
+        <span>E</span>
+        <span>N</span> 
+        <span>U</span>
+      </p>
+
       <svg
         width="307"
         height="306"
@@ -64,19 +63,6 @@ export const ButtonToggleHeader = () => {
         />
       </svg>
 
-      {/* <svg
-        width="384"
-        height="383"
-        viewBox="0 0 384 383"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        id="fa-arrowHeader"
-      >
-        <path
-          d="M70.1114 107.932C71.6451 115.135 78.7271 119.73 85.9294 118.196C93.1317 116.662 97.727 109.581 96.1933 102.378C94.6595 95.1759 87.5776 90.5806 80.3753 92.1143C73.173 93.6481 68.5777 100.73 70.1114 107.932ZM263.386 102.014L265.886 102.014L265.886 99.4701L263.343 99.5144L263.386 102.014ZM263.386 299.823L277.82 285.389L263.386 270.955L248.953 285.389L263.386 299.823ZM83.1959 107.655L263.43 104.514L263.343 99.5144L83.1088 102.656L83.1959 107.655ZM260.886 102.014L260.886 285.389L265.886 285.389L265.886 102.014L260.886 102.014Z"
-          fill="white"
-        />
-      </svg> */}
     </button>
   );
 };
